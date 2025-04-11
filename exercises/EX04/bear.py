702053e9979d58389b7eb1869c57f21d9c956e96
Conrad Eck <ceck@unc.edu>
2025-04-11 16:49:26 -0400

    ex04 finished 2

---
+++ exercises/EX04/bear.py
@@ -0,0 +1,17 @@
+__author__ = "730830842"
+
+"""File to define Bear class."""
+
+
+class Bear:
+
+    def __init__(self):
+        self.age = 0
+        self.hunger_score = 0
+
+    def one_day(self):
+        self.age += 1
+        self.hunger_score -= 1
+
+    def eat(self, num_fish: int) -> None:
+        self.hunger_score += num_fish
---
+++ exercises/EX04/fish.py
@@ -0,0 +1,12 @@
+__author__ = "730830842"
+
+"""File to define Fish class."""
+
+
+class Fish:
+
+    def __init__(self):
+        self.age = 0
+
+    def one_day(self):
+        self.age += 1
